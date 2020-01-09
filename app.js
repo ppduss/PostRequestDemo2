d7cec749f8d42ec95a96d4cba046b9537d71c7ee
Patrick <ppduss@gmail.com>
2020-01-09 16:17:01 -0600

    Filling in more code

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 
 var express = require ("express");
 var app = express();
+var bodyParser = require ("body-parser");
 const port = 3000
+const friends = ["Tony", "Miranda", "Justin", "Pierre", "Lily"];
 
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({extended: true}));
+//
 //
 
 app.get("/", function(req, res){
@@ -12,11 +16,13 @@ app.get("/", function(req, res){
 });
 
 app.post("/addfriend", function(req, res){
-    res.send("post route !");
+    var newFriend = req.body.newfriend;
+    friends.push(newFriend);
+    res.redirect("friends", {friends: friends}); 
+
 })
 
 app.get("/friends", function(req, res){
-    const friends = ["Tony", "Miranda", "Justin", "Pierre", "Lily"];
     res.render("friends", {friends: friends});
 });
 
