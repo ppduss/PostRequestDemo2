62b72bd16c0b539f25056713afb327c9a205b7fd
Patrick <ppduss@gmail.com>
2020-01-09 11:40:41 -0600

    Initializing

--- app.js
+++ app.js
@@ -5,8 +5,19 @@ const port = 3000
 
 app.set("view engine", "ejs");
 
+//
+
 app.get("/", function(req, res){
     res.render("home");
+});
+
+app.post("/addfriend", function(req, res){
+    res.send("post route !");
 })
 
-app.listen(port, () => console.log("Server started !"));
+app.get("/friends", function(req, res){
+    const friends = ["Tony", "Miranda", "Justin", "Pierre", "Lily"];
+    res.render("friends", {friends: friends});
+});
+
+app.listen(port, () => console.log("Server started !")); 
